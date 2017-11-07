a21fb30c0c41cf4849a5b26f5f7364467f92984b
Alejandro Gutierrez <alegtz@AlejandrosMBP2.lan>
2017-11-06 21:34:44 -0600

    first commit

---
+++ webSrvcs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  webSrvcs
+//
+//  Created by Alejandro Gutierrez on 11/6/17.
+//  Copyright © 2017 Alejandro Gutierrez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
