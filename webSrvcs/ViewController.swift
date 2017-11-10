08ca02f4d075bcf8d104de3e1ee4e30031dd6b54
Alejandro Gutierrez <alegtz@AlejandrosMBP2.lan>
2017-11-10 14:45:42 -0600

    search for letra and displays html

--- webSrvcs/ViewController.swift
+++ webSrvcs/ViewController.swift
@@ -11,46 +11,70 @@ import WebKit
 
 class ViewController: UIViewController,WKUIDelegate {
 
-  
-     var webView: WKWebView!
-    
-    override func loadView() {
-        let webConfiguration = WKWebViewConfiguration()
-        webView = WKWebView(frame: .zero, configuration: webConfiguration)
-        webView.uiDelegate = self
-        view = webView
-    }
-
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var textField: UITextField!
+    var word:String?
+    /*URL:
+     https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=json
+     */
+    /*URL:
+     
+     https://es.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=sega
+     
+     */
+//    override func loadView() {
+//        let webConfiguration = WKWebViewConfiguration()
+//        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+//        webView.uiDelegate = self
+//        view = webView
+//    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let myUrl:URL = URL(string: "https://developer.apple.com")!
-        let myRequest  = URLRequest(url: myUrl)
-        //webView.load(myRequest)
+    }
+    @IBAction func search(_ sender: Any)
+    {
+        word = textField.text!
         
-        let task = URLSession.shared.dataTask(with: myRequest)
-        {data,retreive,error in
-            
-            if error == nil
-            {
-                let dataString = NSString(data:data!,encoding:String.Encoding.utf8.rawValue)
-                
-                print(dataString!)
-                
-            }
-            else
+         let apiUrl = "https://es.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=\(word!)"
+        
+        let urlObject = URL(string: apiUrl)
+        
+        let task = URLSession.shared.dataTask(with: urlObject!)
+        {
+            (data,response,error) in
+            if error != nil
             {
                 print(error!)
             }
-            
+            else
+            {
+                do
+                {
+                    let json = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers)
+                    as! [String:Any]
+                    
+                    let querySubJson = json["query"] as! [String:Any]
+                    let queryPages = querySubJson["pages"] as! [String:Any]
+                    print(queryPages)
+                    let queryId = queryPages["993478"] as! [String:Any]
+                    let queryExtract = queryId["extract"] as! String
+                    
+                    DispatchQueue.main.sync {
+                        self.webView.loadHTMLString(queryExtract, baseURL: nil)
+                    }
+                }
+                catch
+                {
+                    print("El procesamiento del JSON tuvo un errorcillo")
+                }
+                
+                
+                    
+            }
         }
         task.resume()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
